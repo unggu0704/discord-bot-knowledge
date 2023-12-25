9f57af152a4a6b89b99f685fcb7830ff48f5d451
UNNGU0704 <kyuhung1001@gmail.com>
2023-12-25 23:39:36 +0900

    create : 최초 커밋

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("net.dv8tion:JDA:5.0.0-beta.4")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
