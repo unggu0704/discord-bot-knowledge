5d673010c6c84b2c6b821fe56f3e0a3855185d29
UNNGU0704 <kyuhung1001@gmail.com>
2023-12-29 19:39:22 +0900

    refactor : 비속처 처리 기능 fillter Class로 이동
    Main 에서 처리하는 비속어 처리기능을 별도의 클래스로 이동

---
+++ src/main/java/Bot/features/MesssagegFilter.java
@@ -0,0 +1,38 @@
+package Bot.features;
+
+import Bot.chatBot.Main;
+import Bot.data.UserData;
+import Bot.data.UserRepository;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+
+import java.util.concurrent.TimeUnit;
+
+public class MesssagegFilter {
+
+    private static final int limitNefativeSpeechCount = 3;
+    public static void profanityFilter(String msg, UserData user, Member target, TextChannel tc, Guild g, UserRepository userRepository) {
+        Main.showMessage(tc, "부정적인 발언이 감지 되었습니다. 지속적으로 사용시 재제됨을 알려드립니다.");
+
+        if (userRepository.findUser(user)) {
+            user.isNegativeSpeechCount();
+            Main.showMessage(tc, user.getUser().getAsMention() + "   (" + user.getNegativeSpeechCount() + "/3)");
+        }
+
+        if (user.getNegativeSpeechCount() >= limitNefativeSpeechCount) {
+            g.ban(target, 60, TimeUnit.SECONDS).queue();
+            Main.showMessage(tc, user.getUser().getAsMention() + "제거 완료!");
+        }
+
+    }
+
+    public static boolean containsNegativeSpeech(String content) {
+        for (NegativeWords speech : NegativeWords.values()) {
+            if (content.contains(speech.name())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
