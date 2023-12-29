2f4c150d0884b6586c55e645e7bde5dec8de77be
UNNGU0704 <kyuhung1001@gmail.com>
2023-12-30 01:19:42 +0900

    feat : 발로란트 게임 참가 리스트 기능 코드 구현 및 main 주석 삭제

--- src/main/java/Bot/features/Command/FrontController.java
+++ src/main/java/Bot/features/Command/FrontController.java
@@ -1,13 +1,19 @@
 package Bot.features.Command;
 
+import Bot.chatBot.Main;
 import Bot.data.UserData;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 public class FrontController {
 
-    public static void handleCommand(String command, UserData user, TextChannel tc, Guild g) {
+    private static final List<ValorantCommand> gameList = new ArrayList<>();
+    public static void handleCommand(String command, UserData user, TextChannel tc, Guild guild) {
         log.info("Command : " + command);
         switch (command) {
             case "도움":
@@ -19,6 +25,36 @@ public class FrontController {
             case "도움2":
                 HelpCommand.special(tc);
                 break;
+            case "발로란트":
+                gameList.add(new ValorantCommand(guild, tc));
+                log.info("발로란트 게임이 시작됨");
+                break;
+            case "동원령":
+                findGuild(gameList, guild).forceStartGame();
+                break;
+            case "참가":
+                findGuild(gameList, guild).joinGame(user);
+                break;
+            case "나가기":
+                findGuild(gameList, guild).exitGame(user);
+                break;
+            case "현재인원":
+                findGuild(gameList, guild).printUserList();
+                break;
+            case "해산":
+                Main.showMessage(tc, "수고하셨습니다.");
+                gameList.remove(findGuild(gameList, guild));
+                break;
         }
+    }
+
+    public static<T> T findGuild(List<T> listByGuild, Guild guild) {
+        for (T thing : listByGuild) {
+            if (thing.equals(guild)) {
+                return thing;
+            }
+        }
+
+        throw new RuntimeException("[ERROR] 찾는 길드가 없습니다.");
     }
 }
--- src/main/java/Bot/features/Command/ValorantCommand.java
+++ src/main/java/Bot/features/Command/ValorantCommand.java
@@ -1,5 +1,86 @@
 package Bot.features.Command;
 
+import Bot.chatBot.Main;
+import Bot.data.UserData;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+
+import java.util.ArrayList;
+import java.util.List;
+@Slf4j
 public class ValorantCommand {
+    private final List<UserData> valorantUserLists = new ArrayList<>();
+
+    private Guild guild;
+    private final TextChannel tc;
+
+    ValorantCommand(Guild guild, TextChannel tc) {
+        Main.showMessage(tc, "발로란트 하실분 @here");
+        this.guild = guild;
+        this.tc = tc;
+    }
+    public  boolean checkSize() {
+        if (valorantUserLists.size() >=5) {
+            Main.showMessage(tc, "5명이 가득핬습니다.");
+            return true;
+        }
+
+        if (valorantUserLists.isEmpty()) {
+            Main.showMessage(tc, "아무도 참가한 사람이 없습니다.");
+            return true;
+        }
+        return false;
+    }
+
+    public  boolean checkDupicate(UserData userData) {
+        return valorantUserLists.contains(userData);
+    }
+    public  void joinGame(UserData userData) {
+        if (checkDupicate(userData) || checkSize())
+            return;
+
+        valorantUserLists.add(userData);
+        Main.showMessage(tc, userData.getUser().getName() + "님 참가 ! \n" + " [ 현재 인원 ] \n");
+        printUserList();
+    }
+
+    public  void exitGame(UserData userData) {
+        if (checkDupicate(userData)) {
+            valorantUserLists.remove(userData);
+            Main.showMessage(tc, userData.getUser().getName() + "님 이 나셨습니다. ! \n" + " [ 현재 인원 ] \n");
+            printUserList();
+        }
+
+        Main.showMessage(tc, "팀에 참가 되어 있지 않은 유저입니다.");
+    }
+
+
+    public  void forceStartGame() {
+        Main.showMessage(tc, "@everyone");
+    }
+
+    public  void printUserList() {
+        StringBuilder sb = new StringBuilder();
+
+        for (UserData userData : valorantUserLists) {
+            sb.append(userData.getName()).append("\n");
+        }
+
+        Main.showBuilderMeesage(tc, new EmbedBuilder().setTitle(sb.toString()));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        ValorantCommand that = (ValorantCommand) obj;
+        return this.guild.equals(that.guild);
+    }
+
 
 }
