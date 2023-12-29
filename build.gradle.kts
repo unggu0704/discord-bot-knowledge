5d3d112e5c60fa59c5fb7111752da77fbaabe8f2
UNNGU0704 <kyuhung1001@gmail.com>
2023-12-29 19:42:42 +0900

    refactor : log 처리를 위해 lombok 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.projectlombok:lombok:1.18.28")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("net.dv8tion:JDA:5.0.0-beta.4")
