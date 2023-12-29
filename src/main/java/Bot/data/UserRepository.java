13675db02afc2379c69cde2b1d3e12b9dc3b21d0
UNNGU0704 <kyuhung1001@gmail.com>
2023-12-29 19:38:39 +0900

    feat : userReository 구현
    기존의 List로 유저 정보를 저장하고 있는 것을 userRepository라는 Class를 생성해 구현

---
+++ src/main/java/Bot/data/UserRepository.java
@@ -0,0 +1,74 @@
+package Bot.data;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class UserRepository {
+    private final String csvFilePath = "UserData.csv";
+    private final List<UserData> userReository = new ArrayList<>();
+
+
+    public UserRepository() {
+        loadCSVUser();
+    }
+
+    public void saveUser(UserData userData) {
+        userReository.add(userData);
+    }
+
+    public void getUser() {
+    }
+
+    public void updateUser() {
+
+    }
+
+    public int getUserCount() {
+        return userReository.size();
+    }
+
+    public boolean findUser(UserData userdata) {
+        return userReository.contains(userdata);
+    }
+
+    public void loadCSVUser() {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(csvFilePath))) {
+            String name;
+            reader.readLine();
+            while ((name = reader.readLine()) != null) {
+                UserData userInfo = new UserData(name);
+                userReository.add(userInfo);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveCSVUser() {
+        try (FileWriter writer = new FileWriter(csvFilePath)) {
+
+            for (UserData user : userReository) {
+                log.info("User 저장완료");
+                writer.write(user.getName() + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void printUser() {
+        for(UserData user : userReository) {
+            System.out.println(user.getName());
+        }
+    }
+
+
+}
