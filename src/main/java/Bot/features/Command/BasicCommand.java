e61b6033c9c9eed06b3d4fe7b99969c726711657
UNNGU0704 <kyuhung1001@gmail.com>
2024-01-06 19:14:04 +0900

    feat : BasicComand 추가

---
+++ src/main/java/Bot/features/Command/BasicCommand.java
@@ -0,0 +1,10 @@
+package Bot.features.Command;
+
+import Bot.data.UserData;
+import Bot.data.UserRepository;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+
+public interface BasicCommand {
+    void execute(UserRepository userRepository, UserData user, TextChannel tc, Guild guild, String commaand);
+}
