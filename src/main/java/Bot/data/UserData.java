8ba888b9045f86ac485ae0a20a0fa98ced2b3a98
UNNGU0704 <kyuhung1001@gmail.com>
2024-01-13 21:47:42 +0900

    refactor : InvalidDefinitionException 해결 시도 setter 추가 및 jackson 설정

--- src/main/java/Bot/data/UserData.java
+++ src/main/java/Bot/data/UserData.java
@@ -1,21 +1,30 @@
 package Bot.data;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.Getter;
+import lombok.Setter;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class UserData {
     @Getter
+    @Setter
     private User user;
     @Getter
+    @Setter
     private Member member;
     @Getter
+    @Setter
     private int negativeSpeechCount = 0;
     @Getter
+    @Setter
     private String name;
     @Getter
+    @Setter
     private String id;
     @Getter
+    @Setter
     private String discriminator;
 
     public UserData() {
